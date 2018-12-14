3cda03d4bee0adc7e9c752312fda78948b2f8d6d
babelfor.net <info@babelfor.net>
2018-12-14 09:58:34 +0100

    Project Setup

---
+++ AppVeyorIntegration/AppVeyorIntegration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppVeyorIntegration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
